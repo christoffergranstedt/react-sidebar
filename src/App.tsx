e07e75ac233192b2ae3427095c31a136f84bb615
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2022-02-01 18:20:48 +0100

    adding tailwind react router dom and empty homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HomePage } from './Pages/HomePage/HomePage'
 
 function App () {
   return (
-    <div className="App">
-      <h1>Hej</h1>
+    <div className="bg-gray-800 h-screen text-white">
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
