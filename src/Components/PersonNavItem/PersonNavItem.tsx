bc8234341435b60e8169ece09f4ebc868ab14f45
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2022-02-03 21:32:33 +0100

    small update on sidebar and navitems

--- src/Components/PersonNavItem/PersonNavItem.tsx
+++ src/Components/PersonNavItem/PersonNavItem.tsx
@@ -21,17 +21,15 @@ export const PersonNavItem: React.FC<PersonNavItemProps> = ({ className = '', pe
   const shouldShowChildrens = isExpanded && hasChildrens
 
   return (
-    <>
-    <div className={`${className} ${selectedPerson?.id === personInfo.id && 'bg-gray-600'} rounded-md relative py-4 hover:bg-gray-600`}>
-      <Link to="#" onClick={selectPersonNavItem} className="hover:text-gray-200 pl-2">{personInfo.name}</Link>
-      {hasChildrens && <Link to="#" className="text-yellow-600 absolute right-2 cursor-pointer hover:text-yellow-400 font-bold inline-block" onClick={() => setIsExpanded(!isExpanded)}>{!isExpanded ? <span>&#8910;</span> : <span>&#8911;</span>} </Link>}
-    </div>
-
-    {shouldShowChildrens && personInfo.children.map(person => (
-      <div key={`sidebar-nav-${person.id}`} className="pl-4">
-        <PersonNavItem className="my-1" personInfo={person}/>
+    <div className={className}>
+      <div className={`${className} ${selectedPerson?.id === personInfo.id && 'bg-gray-600'} rounded-md relative py-4 hover:bg-gray-600`}>
+        <Link to="#" onClick={selectPersonNavItem} className="hover:text-gray-200 pl-2">{personInfo.name}</Link>
+        {hasChildrens && <Link to="#" className="text-yellow-600 absolute right-2 cursor-pointer hover:text-yellow-400 font-bold inline-block" onClick={() => setIsExpanded(!isExpanded)}>{!isExpanded ? <span>&#8910;</span> : <span>&#8911;</span>} </Link>}
       </div>
-    ))}
-    </>
+
+      {shouldShowChildrens && personInfo.children.map(person => (
+        <PersonNavItem key={`sidebar-nav-${person.id}`} className="my-1 pl-4" personInfo={person}/>
+      ))}
+    </div>
   )
 }
--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -14,7 +14,7 @@ export const SideBar: React.FC<SideBarProps> = ({ className = '' }) => {
     <nav className={`${className} px-2 text-lg text-gray-300 font-semibold`}>
       {isLoading && <div className="flex justify-center"><Bars color="#CA8A04" height={40} width={40} /></div>}
 
-      {persons.map(personInfo => (
+      {!isLoading && persons.map(personInfo => (
         <div key={`sidebar-nav-${personInfo.id}`} className="bg-gray-800 rounded-md p-1">
           <PersonNavItem className="bg-gray-800" personInfo={personInfo}/>
         </div>
